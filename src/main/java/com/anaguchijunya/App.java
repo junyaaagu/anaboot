f592fb73f27a10e3c519e8e43563491493d57566
junyaagu <code@anaguchijunya.com>
2015-06-30 17:58:45 +0900

    JPAページ処理

--- src/main/java/com/anaguchijunya/App.java
+++ src/main/java/com/anaguchijunya/App.java
@@ -1,10 +1,15 @@
 package com.anaguchijunya;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.anaguchijunya.domain.Customer;
 import com.anaguchijunya.repository.CustomerRepository;
@@ -19,13 +24,21 @@ public class App implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
+		// 追加
+		customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
 
-		// 1件投入
-		Customer customer = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
-		System.out.println("customer=" + customer);
+		// 全件名前順で表示する
+//		customerRepository.findAllOrderByName().forEach(c -> System.out.println("名前順：" + c.toString()));
 
-		// 全件表示する
-		customerRepository.findAll().forEach(System.out::println);
+		// ページング処理(全件、カスタマイズなし）
+		Pageable pagable = new PageRequest(0, 3);
+		Page<Customer> page = customerRepository.findAll(pagable);
+		page.getContent().forEach(System.out::println);
+
+		// ページング処理（全件、カスタマイズ）
+		Pageable pagable2 = new PageRequest(0, 3);
+		List<Customer> page2 = customerRepository.findAllOrderByName(pagable2);
+		page2.forEach(System.out::println);
 
 	}
 
--- src/main/java/com/anaguchijunya/repository/CustomerRepository.java
+++ src/main/java/com/anaguchijunya/repository/CustomerRepository.java
@@ -1,10 +1,23 @@
 package com.anaguchijunya.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.anaguchijunya.domain.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 
-	// インターフェースを作っておくとインスタンスをコンテナ（Spring)が作ってくれる
+	// インターフェースを作っておくとインスタンスをコンテナ（Spring)が作ってくれる.
+	// 自分でカスタマイズしたい場合は以下のようにQueryアノテーションを付けてJPQLを記述する
+
+	@Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
+	List<Customer> findAllOrderByName() ;
+
+	@Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
+	List<Customer> findAllOrderByName(Pageable pageable) ;
+
+
 }
